730001d97adc7e0b2c4fb13188d48afe5881ffa8
jason <jduong89@gmail.com>
2022-09-02 19:03:59 -0700

    adds css flex formatting and buttons for adding budget and expense"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Button, Container, Stack } from "react-bootstrap";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budget</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      ></div>
+    </Container>
+  );
 }
 
 export default App;
