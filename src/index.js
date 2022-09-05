86b05856162b366f8343bfc5ec94758569365248
jason <jduong89@gmail.com>
2022-09-05 13:53:23 -0700

    wraps budgetprovider around app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BudgetProvider } from "./contexts/BudgetsContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BudgetProvider>
+      <App />
+    </BudgetProvider>
   </React.StrictMode>
 );
