761605a9bb1bdcc40b84bf8c454d3317f048e798
jason <jduong89@gmail.com>
2022-09-05 14:18:12 -0700

    addresses spelling

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BudgetProvider } from "./contexts/BudgetsContext";
+import { BudgetsProvider } from "./contexts/BudgetsContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BudgetProvider>
+    <BudgetsProvider>
       <App />
-    </BudgetProvider>
+    </BudgetsProvider>
   </React.StrictMode>
 );
